26770c394f0875650ae3407fd5b511b11b33d6b2
sirius97 <sirimukund@gmail.com>
2020-05-02 19:54:05 +0530

    responsive update

--- src/Container/ToBuy/Item/Item.js
+++ src/Container/ToBuy/Item/Item.js
@@ -6,14 +6,17 @@ const item = (props) => {
 
       return  (
             <div className={classes.Item}>
+                <div style = {{display:'block'}}>
                 <div className ={classes.A}>
                     <div>
                     <input type="text" onChange = {props.changeName} placeholder="Enter here" value={props.name}/>
                     </div>
                     <QuantityCounter quantity = {props.quantity} increment = {props.increment} decrement = {props.decremet} />
                 </div>
+                
                 <button onClick={props.delete} className = {classes.Delete}>Delete</button>
                 <button onClick = {props.done} className={classes.Done}>Done</button> 
+                </div>
             </div>
         );
 
